5271f7dee31b10d701c254e5a565ebd21147b753
pimg <pim@rubix.nl>
2016-08-01 19:23:06 +0200

    changed git repo in kubernetes json and added JBoss Fuse repos to bash script

--- nexus/nexus-container/scripts/addjbossrepos.sh
+++ nexus/nexus-container/scripts/addjbossrepos.sh
@@ -4,3 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ${DIR}/addrepo.sh jboss http://maven.repository.redhat.com/techpreview/all/
 ${DIR}/addrepo.sh jboss-ce https://repository.jboss.org/nexus/content/groups/public/
+${DIR}/addrepo.sh fusesource-m2 https://repo.fusesource.com/nexus/content/groups/public
+${DIR}/addrepo.sh fusesource-ea https://repo.fusesource.com/nexus/content/groups/ea
+${DIR}/addrepo.sh redhat-ga https://maven.repository.redhat.com/ga
